b54127c49f799972c9fd2ec3d3ff7a80dc6748d7
Artemiy <a.mazaew@lambda-it.ru>
2022-12-17 23:51:43 +0300

    Обновил файл установки библиотеки

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('./requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='tensorairspace',
-      version='0.0.1',
-      install_requires=[
-          'tensorflow==2.2.0',
-          'gym',
-          'tqdm',
-          'matplotlib',
-          'matlab',
-      ]
+        version='0.1.1',
+        install_requires=reqs,
+        python_requires=">=3.7",
+        description="TensorAirSpace! - RL for Aerospace.",
+        url="https://github.com/TensorAirSpace/TensorAirSpace",
+        keywords="reinforcement-learning machine-learning gym openai aerospace toolbox python data-science",
       )
