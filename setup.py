3ca20ec278fb9d49aa41f677e6d77a923fa53477
mr8bit <a.mazaew@lambda-it.ru>
2023-05-28 20:02:54 +0300

    Изменил версию либы

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='tensoraerospace',
-        version='0.1.1',
+        version='0.2.1',
         install_requires=required,
         packages=['tensoraerospace'],
         python_requires=">=3.7",
