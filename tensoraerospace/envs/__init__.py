16fdad5a06f19413f63b6ba18b86d7bb51c1d8ed
Artemiy <asmazaev@cloud.ru>
2024-04-16 02:59:42 +0300

    Обновил ComSat до gymnasium

--- tensoraerospace/envs/__init__.py
+++ tensoraerospace/envs/__init__.py
@@ -2,7 +2,7 @@ from .f16.linear_longitudial import LinearLongitudinalF16
 from .b747 import LinearLongitudinalB747
 # from .elv import LinearLongitudinalELVRocket
 # from .rocket import LinearLongitudinalMissileModel
-# from .comsat import ComSatEnv
+from .comsat import ComSatEnv
 # from .geostat import GeoSatEnv
 # from .f4c import LinearLongitudinalF4C
 # from .x15 import LinearLongitudinalX15
--- tensoraerospace/envs/comsat.py
+++ tensoraerospace/envs/comsat.py
@@ -1,11 +1,11 @@
 import gymnasium as gym
 import numpy as np
-from gym.utils import EzPickle
 from tensoraerospace.aerospacemodel import ComSat
+from gymnasium import spaces
 
 
 
-class ComSatEnv(gym.Env, EzPickle):
+class ComSatEnv(gym.Env):
     """Моделирование объекта управления "Спутник связи в продольном канале управления" в среде моделирования OpenAI Gym для обучения агентов с искусственным интеллектом
 
     Args:
@@ -26,8 +26,7 @@ class ComSatEnv(gym.Env, EzPickle):
                  control_space=['thrust'],
                  output_space=["rho", "theta", "omega"],
                  reward_func=None):
-
-        EzPickle.__init__(self)
+        self.max_action_value = 25.0
         self.initial_state = initial_state
         self.number_time_steps = number_time_steps
         self.selected_state_output = output_space
@@ -48,6 +47,15 @@ class ComSatEnv(gym.Env, EzPickle):
         self.ref_signal = reference_signal
         self.model.initialise_system(x0=initial_state, number_time_steps=number_time_steps)
         self.number_time_steps = number_time_steps
+
+        self.action_space = spaces.Box(low=-60, high=60, shape=(len(control_space),1), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(len(state_space),1), dtype=np.float32)
+
+        self.current_step = 0
+        self.done = False
+        
+    def _get_info(self):
+        return {}
     
     @staticmethod
     def reward(state, ref_signal, ts):
@@ -75,20 +83,27 @@ class ComSatEnv(gym.Env, EzPickle):
             done (bool): Статус моделирования, завершено или нет
             logging (any): Дополнительная информацию (не используется)
         """
+        self.current_step += 1
         next_state = self.model.run_step(action)
-        reward = self.reward_func(next_state[self.indices_tracking_states], self.ref_signal, self.model.time_step)
-        if self.model.time_step == self.number_time_steps:
-            return next_state, reward, True, {}
-        return next_state, reward, False, {}
+        reward = self.reward_func(next_state[self.indices_tracking_states], self.reference_signal, self.current_step)
+        self.done = self.current_step >= self.number_time_steps - 2
+        info = self._get_info()
+
+        return next_state.reshape([1,-1])[0], reward, self.done, False, info
 
     def reset(self):
         """Восстановление среды моделирования в начальные условия
-        """
+        """       
+        self.current_step = 0
+        self.done = False
+
         self.model = None
         self.model = ComSat(self.initial_state, number_time_steps=self.number_time_steps,
                                      selected_state_output=self.output_space, t0=0)
         self.ref_signal = self.reference_signal
         self.model.initialise_system(x0=self.initial_state, number_time_steps=self.number_time_steps)
+        info = self._get_info()
+        return np.array(self.initial_state, dtype=np.float64)[self.model.selected_state_index].reshape([1,-1])[0], info
 
     def render(self):
         """Визуальное отображение действий в среде. В статусе WIP
