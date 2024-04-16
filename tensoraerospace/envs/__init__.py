95140cdd8352da4118d16ea138a05cec6cfe8162
Artemiy <a.mazaew@lambda-it.ru>
2024-04-16 03:24:18 +0300

    Добавил gymnasium в LinearLongitudinalELVRocket и тесты в него же

--- tensoraerospace/envs/__init__.py
+++ tensoraerospace/envs/__init__.py
@@ -1,6 +1,6 @@
 from .f16.linear_longitudial import LinearLongitudinalF16
 from .b747 import LinearLongitudinalB747
-# from .elv import LinearLongitudinalELVRocket
+from .elv import LinearLongitudinalELVRocket
 from .rocket import LinearLongitudinalMissileModel
 from .comsat import ComSatEnv
 # from .geostat import GeoSatEnv
