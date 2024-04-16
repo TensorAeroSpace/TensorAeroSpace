f0e647ad2ea0e24f6b39b554a719083ff87cec51
Artemiy <asmazaev@cloud.ru>
2024-04-16 03:07:44 +0300

    Добавил

--- tensoraerospace/envs/__init__.py
+++ tensoraerospace/envs/__init__.py
@@ -1,7 +1,7 @@
 from .f16.linear_longitudial import LinearLongitudinalF16
 from .b747 import LinearLongitudinalB747
 # from .elv import LinearLongitudinalELVRocket
-# from .rocket import LinearLongitudinalMissileModel
+from .rocket import LinearLongitudinalMissileModel
 from .comsat import ComSatEnv
 # from .geostat import GeoSatEnv
 # from .f4c import LinearLongitudinalF4C
