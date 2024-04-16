287cf8a6517d05ab7473eaebb3d92ae920570e27
Artemiy <a.mazaew@lambda-it.ru>
2024-04-16 03:30:19 +0300

    Делаем базовый класс для всех агнетов что бы привести их в порядок и выгрузить их в HuggigFace

---
+++ tensoraerospace/agent/base.py
@@ -0,0 +1,20 @@
+from abc import ABC
+
+
+class BaseRLModel(ABC):
+
+    def __init__(self) -> None:
+        super().__init__()
+        pass
+    
+    def get_env():
+        pass
+    
+    def train():
+        pass
+    
+    def action_probability():
+        pass
+    
+    def save():
+        pass
