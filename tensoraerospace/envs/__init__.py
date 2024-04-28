158b58c70bdc847cc8726520ec16efd2220e95b7
Artemiy <a.mazaew@lambda-it.ru>
2024-04-28 03:07:44 +0300

    Добавил тесты для остальных сред

--- tensoraerospace/envs/__init__.py
+++ tensoraerospace/envs/__init__.py
@@ -5,8 +5,8 @@ from .rocket import LinearLongitudinalMissileModel
 from .comsat import ComSatEnv
 from .lapan import LinearLongitudinalLAPAN
 from .ultrastick import LinearLongitudinalUltrastick
-# from .geostat import GeoSatEnv
-# from .f4c import LinearLongitudinalF4C
-# from .x15 import LinearLongitudinalX15
-# from  .uav import LinearLongitudinalUAV
+from .geostat import GeoSatEnv
+from .f4c import LinearLongitudinalF4C
+from .x15 import LinearLongitudinalX15
+from  .uav import LinearLongitudinalUAV
 # from .unity_env import get_plane_env, unity_discrete_env
