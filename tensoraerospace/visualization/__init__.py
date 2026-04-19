099c2068cc7ca924a4b3bc8a2bc855ab9da4cf80
Artemiy Mazaew <a.mazaew@lambda-it.ru>
2026-04-20 02:42:54 +0300

    feat(viz): add Plotly figure builder for 3D flight visualization

--- tensoraerospace/visualization/__init__.py
+++ tensoraerospace/visualization/__init__.py
@@ -1,11 +1,13 @@
 """Flight visualization utilities (3D trail + synced charts)."""
 
+from .flight_3d import build_flight_3d_figure
 from .kinematics import (
     reconstruct_position_6dof,
     reconstruct_position_longitudinal,
 )
 
 __all__ = [
+    "build_flight_3d_figure",
     "reconstruct_position_6dof",
     "reconstruct_position_longitudinal",
 ]
